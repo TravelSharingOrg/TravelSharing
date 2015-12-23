3ed0d73ed40c4a683697f4f6df6eb41d319babdb
K-Iliev <konstantin.iliev92@gmail.com>
2015-12-24 00:41:07 +0200

    TravelSharing.Data.Models and TravelSharing.Data class libraries added

---
+++ Source/Data/TravelSharing.Data/TravelSharingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelSharing.Data.Models;
+
+namespace TravelSharing.Data
+{
+    public class TravelSharingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TravelSharingDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TravelSharingDbContext Create()
+        {
+            return new TravelSharingDbContext();
+        }
+    }
+}
--- Source/Web/TravelSharing.Web/Controllers/AccountController.cs
+++ Source/Web/TravelSharing.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using TravelSharing.Web.Models;
+using TravelSharing.Data.Models;
 
 namespace TravelSharing.Web.Controllers
 {
